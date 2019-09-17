41628bd418bf3c33a4aa2f19be71ed5bb7caf9c7
Zaldy Baguinon (@IT) <zaldy.baguinon@vdimdci.com.ph>
2019-09-17 17:24:01 +0800

    Initial Commit

---
+++ replicator.go
@@ -0,0 +1,329 @@
+package replicator
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/eaglebush/datahelper"
+)
+
+// Replicator - A replicator for message-given data
+type Replicator struct {
+	AutodetectTable bool
+	Subjects        []Item
+}
+
+// Column - replication column for initialization of replicator
+type Column struct {
+	Name string
+	Type string
+	Null bool
+}
+
+// Item - an item in the replicator
+type Item struct {
+	SubjectRoot string
+	Columns     []Column
+	DataKeys    []string
+}
+
+var subjectTable map[string]string
+var dataKeys map[string][]string
+
+// Init - initialize table
+func (r *Replicator) Init(dh *datahelper.DataHelper, subject string, tableColumns []Column, dataKeyColumns []string) error {
+	subject = strings.ToLower(subject)
+
+	// Check subject composition. Default composition should be in <api>.<table>.<event> format. There must be 2 dots in the subject
+	tbln := strings.Split(subject, `.`)
+	if len(tbln) != 3 {
+		return errors.New(`The subject is not compliant with the name convention. Convention should be in <api>.<table>.<event> format`)
+	}
+
+	// create a table name from the event/subject
+	tableName := tbln[0] + `.` + tbln[1]
+
+	// Check if table is present in the subjectTable map
+	for k := range subjectTable {
+		if k == tableName {
+			return nil
+		}
+	}
+
+	for _, rep := range []string{`.`, `,`} {
+		tableName = strings.Replace(tableName, rep, `_`, -1)
+	}
+
+	if subjectTable == nil {
+		subjectTable = make(map[string]string)
+		dataKeys = make(map[string][]string)
+	}
+	subjectTable[tableName] = tableName
+	dataKeys[tableName] = dataKeyColumns
+
+	cnt := len(tableColumns) - 1
+	sql := ``
+	sql += `CREATE TABLE ` + tableName + ` (`
+	for i, v := range tableColumns {
+		sql += `	` + v.Name + ` ` + v.Type + ` `
+		if v.Null {
+			sql += `NOT `
+		}
+		sql += `NULL`
+
+		if cnt != i {
+			sql += `,`
+		}
+		sql += "\r\n"
+	}
+	sql += `);`
+
+	// create table it does not exist
+	_, err := dh.Exec(sql)
+	if err != nil {
+		// table might have existed
+		log.Printf("Error Init: %s", err.Error())
+	}
+
+	return nil
+}
+
+// Insert - insert action to database
+func (r *Replicator) Insert(dh *datahelper.DataHelper, subject string, msgData []byte) error {
+	// Check subject composition. Default composition should be in <api>.<table>.<event> format. There must be 2 dots in the subject
+	tbln := strings.Split(subject, `.`)
+	if len(tbln) != 3 {
+		return errors.New(`The subject is not compliant with the name convention. Convention should be in <api>.<table>.<event> format`)
+	}
+
+	// create a table name from the event/subject
+	tableName := tbln[0] + `.` + tbln[1]
+
+	for _, rep := range []string{`.`, `,`} {
+		tableName = strings.Replace(tableName, rep, `_`, -1)
+	}
+
+	var objmap map[string]interface{}
+	err := json.Unmarshal(msgData, &objmap)
+	if err != nil {
+		return err
+	}
+
+	cma := ``
+	cols := ``
+	vals := ``
+	sql := ``
+
+	for k, v := range objmap {
+		cols += cma + k
+		vals += cma + `'` + strings.Replace(anytstr(v), `'`, `''`, -1) + `'`
+		cma = `, `
+	}
+
+	tbl := subjectTable[tableName]
+	if len(tbl) == 0 {
+		return errors.New(`The table name could not be found from the given subject`)
+	}
+
+	sql += `INSERT INTO ` + tbl + ` (` + cols + `) VALUES (` + vals + `);`
+
+	log.Println(sql)
+
+	_, err = dh.Exec(sql)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Update - update action to database
+func (r *Replicator) Update(dh *datahelper.DataHelper, subject string, msgData []byte) error {
+	// Check subject composition. Default composition should be in <api>.<table>.<event> format. There must be 2 dots in the subject
+	tbln := strings.Split(subject, `.`)
+	if len(tbln) != 3 {
+		return errors.New(`The subject is not compliant with the name convention. Convention should be in <api>.<table>.<event> format`)
+	}
+
+	// create a table name from the event/subject
+	tableName := tbln[0] + `.` + tbln[1]
+
+	for _, rep := range []string{`.`, `,`} {
+		tableName = strings.Replace(tableName, rep, `_`, -1)
+	}
+
+	var objmap map[string]interface{}
+	err := json.Unmarshal(msgData, &objmap)
+
+	cma := ``
+	colvals := ``
+	sql := ``
+	valid := true
+
+	var dataKeyValues map[string]interface{}
+	dataKeyValues = make(map[string]interface{})
+
+	for k, v := range objmap {
+		valid = true
+
+		// check if the column is in the datakeys
+		for _, f := range dataKeys[tableName] {
+			if f == k {
+				dataKeyValues[f] = v
+				valid = false
+			}
+		}
+
+		if valid {
+			colvals += cma + k + `='` + strings.Replace(anytstr(v), `'`, `''`, -1) + `'`
+			cma = `, `
+		}
+
+	}
+
+	tbl := subjectTable[tableName]
+	if len(tbl) == 0 {
+		return errors.New(`The table name could not be found from the given subject`)
+	}
+
+	sql += `UPDATE ` + tbl + ` SET ` + colvals + ` WHERE `
+
+	// Get filters
+	cma = ``
+	for k, v := range dataKeyValues {
+		sql += cma + k + `='` + strings.Replace(anytstr(v), `'`, `''`, -1) + `'`
+		cma = ` AND `
+	}
+
+	_, err = dh.Exec(sql)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete - delete a record in the database
+func (r *Replicator) Delete(dh *datahelper.DataHelper, subject string, msgData []byte) error {
+	// Check subject composition. Default composition should be in <api>.<table>.<event> format. There must be 2 dots in the subject
+	tbln := strings.Split(subject, `.`)
+	if len(tbln) != 3 {
+		return errors.New(`The subject is not compliant with the name convention. Convention should be in <api>.<table>.<event> format`)
+	}
+
+	// create a table name from the event/subject
+	tableName := tbln[0] + `.` + tbln[1]
+
+	for _, rep := range []string{`.`, `,`} {
+		tableName = strings.Replace(tableName, rep, `_`, -1)
+	}
+
+	var objmap map[string]interface{}
+	err := json.Unmarshal(msgData, &objmap)
+
+	cma := ``
+	sql := ``
+
+	var dataKeyValues map[string]interface{}
+	dataKeyValues = make(map[string]interface{})
+
+	for k, v := range objmap {
+		// check if the column is in the datakeys
+		for _, f := range dataKeys[tableName] {
+			if f == k {
+				dataKeyValues[f] = v
+			}
+		}
+	}
+
+	sql += `DELETE FROM ` + subjectTable[tableName] + ` WHERE `
+
+	// Get filters
+	cma = ``
+	for k, v := range dataKeyValues {
+		sql += cma + k + `='` + strings.Replace(anytstr(v), `'`, `''`, -1) + `'`
+		cma = ` AND `
+	}
+
+	_, err = dh.Exec(sql)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// LoadReplicator - load replicator table definitions from configuration file
+func LoadReplicator(dh *datahelper.DataHelper, replicatorConfig string) (*Replicator, error) {
+	b, err := ioutil.ReadFile(replicatorConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	var rp Replicator
+	err = json.Unmarshal(b, &rp)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range rp.Subjects {
+		//rpi := v.(ReplicatorItem)
+		err = rp.Init(dh, v.SubjectRoot, v.Columns, v.DataKeys)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &rp, nil
+}
+
+func anytstr(value interface{}) string {
+	var b string
+
+	switch value.(type) {
+	case string:
+		b = value.(string)
+	case int:
+		b = strconv.FormatInt(int64(value.(int)), 10)
+	case int8:
+		b = strconv.FormatInt(int64(value.(int8)), 10)
+	case int16:
+		b = strconv.FormatInt(int64(value.(int16)), 10)
+	case int32:
+		b = strconv.FormatInt(int64(value.(int32)), 10)
+	case int64:
+		b = strconv.FormatInt(value.(int64), 10)
+	case uint:
+		b = strconv.FormatUint(uint64(value.(uint)), 10)
+	case uint8:
+		b = strconv.FormatUint(uint64(value.(uint8)), 10)
+	case uint16:
+		b = strconv.FormatUint(uint64(value.(int16)), 10)
+	case uint32:
+		b = strconv.FormatUint(uint64(value.(int32)), 10)
+	case uint64:
+		b = strconv.FormatUint(uint64(value.(uint64)), 10)
+	case float32:
+		b = fmt.Sprintf("%f", value.(float32))
+	case float64:
+		b = fmt.Sprintf("%f", value.(float64))
+	case bool:
+		b = "0"
+		s := strings.ToLower(value.(string))
+		if len(s) > 0 {
+			if s == "true" || s == "on" || s == "yes" || s == "1" || s == "-1" {
+				b = "1"
+			}
+		}
+	case time.Time:
+		b = "'" + value.(time.Time).Format(time.RFC3339) + "'"
+	}
+
+	return b
+}
---
+++ replicator_test.go
@@ -0,0 +1,234 @@
+package replicator
+
+import (
+	"log"
+	"testing"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	cfg "github.com/eaglebush/config"
+	"github.com/eaglebush/datahelper"
+)
+
+func TestInitReplicator(t *testing.T) {
+	var r Replicator
+	r = Replicator{}
+
+	config, err := cfg.LoadConfig(`./config.json`)
+	if err != nil {
+		log.Fatal("Configuration file not found!")
+	}
+
+	// connect to database
+	dh := datahelper.NewDataHelper(config)
+	_, err = dh.Connect()
+	defer dh.Disconnect()
+	if err != nil {
+		return
+	}
+
+	// define table
+	err = r.Init(dh, `frt.freight`, []Column{
+		Column{
+			Name: `freight_key`,
+			Type: `nvarchar(38)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_address`,
+			Type: `nvarchar(400)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_amount`,
+			Type: `decimal(18,3)`,
+			Null: false,
+		},
+	}, []string{
+		`freight_key`,
+	})
+
+	if err != nil {
+		log.Printf("Error found: %s\r\n", err.Error())
+	}
+}
+
+func TestInsertReplicator(t *testing.T) {
+	var r Replicator
+	r = Replicator{}
+
+	config, err := cfg.LoadConfig(`./config.json`)
+	if err != nil {
+		log.Fatal("Configuration file not found!")
+	}
+
+	// connect to database
+	dh := datahelper.NewDataHelper(config)
+	_, err = dh.Connect()
+	defer dh.Disconnect()
+	if err != nil {
+		return
+	}
+
+	// define table
+	err = r.Init(dh, `frt.freight`, []Column{
+		Column{
+			Name: `freight_key`,
+			Type: `nchar(38)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_address`,
+			Type: `nvarchar(400)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_amount`,
+			Type: `decimal(18,3)`,
+			Null: false,
+		},
+	}, []string{
+		`freight_key`,
+	})
+
+	if err != nil {
+		log.Printf("Error found: %s\r\n", err.Error())
+	}
+
+	data := `{
+				"freight_key": "1QuIyNTdgyCQtxHeVuHxROrtw3B           ",
+				"ref_address": "21 Jump St. Manila",
+				"ref_amount": 1.10
+			 }`
+
+	err = r.Insert(dh, `frt.freight.freightcreated`, []byte(data))
+	if err != nil {
+		log.Printf("Error found: %s\r\n", err.Error())
+	}
+}
+
+func TestUpdateReplicator(t *testing.T) {
+	var r Replicator
+	r = Replicator{}
+
+	config, err := cfg.LoadConfig(`./config.json`)
+	if err != nil {
+		log.Fatal("Configuration file not found!")
+	}
+
+	// connect to database
+	dh := datahelper.NewDataHelper(config)
+	_, err = dh.Connect()
+	defer dh.Disconnect()
+	if err != nil {
+		return
+	}
+
+	// define table
+	err = r.Init(dh, `frt.freight`, []Column{
+		Column{
+			Name: `freight_key`,
+			Type: `nchar(38)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_address`,
+			Type: `nvarchar(400)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_amount`,
+			Type: `decimal(18,3)`,
+			Null: false,
+		},
+	}, []string{
+		`freight_key`,
+	})
+
+	if err != nil {
+		log.Printf("Error found: %s\r\n", err.Error())
+	}
+
+	data := `{
+				"freight_key": "1QuIyNTdgyCQtxHeVuHxROrtw3B           ",
+				"ref_address": "21 Jump St. Quezon City",
+				"ref_amount": 1.12
+			 }`
+
+	err = r.Update(dh, `frt.freight`, []byte(data))
+	if err != nil {
+		log.Printf("Error found: %s\r\n", err.Error())
+	}
+}
+
+func TestDeleteReplicator(t *testing.T) {
+	var r Replicator
+	r = Replicator{}
+
+	config, err := cfg.LoadConfig(`./config.json`)
+	if err != nil {
+		log.Fatal("Configuration file not found!")
+	}
+
+	// connect to database
+	dh := datahelper.NewDataHelper(config)
+	_, err = dh.Connect()
+	defer dh.Disconnect()
+	if err != nil {
+		return
+	}
+
+	// define table
+	err = r.Init(dh, `frt.freight`, []Column{
+		Column{
+			Name: `freight_key`,
+			Type: `nchar(38)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_address`,
+			Type: `nvarchar(400)`,
+			Null: false,
+		},
+		Column{
+			Name: `ref_amount`,
+			Type: `decimal(18,3)`,
+			Null: false,
+		},
+	}, []string{
+		`freight_key`,
+	})
+
+	if err != nil {
+		log.Printf("Error found: %s\r\n", err.Error())
+	}
+
+	data := `{
+				"freight_key": "1QuIyNTdgyCQtxHeVuHxROrtw3B           ",
+				"ref_address": "21 Jump St. Quezon City",
+				"ref_amount": 1.10
+			 }`
+
+	err = r.Delete(dh, `frt.freight`, []byte(data))
+	if err != nil {
+		log.Printf("Error found: %s\r\n", err.Error())
+	}
+}
+
+func TestLoadReplicator(t *testing.T) {
+
+	config, err := cfg.LoadConfig(`./config.json`)
+	if err != nil {
+		log.Fatal("Configuration file not found!")
+	}
+
+	// connect to database
+	dh := datahelper.NewDataHelper(config)
+	_, err = dh.Connect()
+	defer dh.Disconnect()
+	if err != nil {
+		return
+	}
+
+	rp, err := LoadReplicator(dh, `./replicator.json`)
+	log.Printf("Replicators: %v", rp)
+}
